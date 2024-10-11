1d6eec104221b5c19d64fd210bf6d958af7456f9
Safran Quader <safran.quader@gmail.com>
2024-10-12 00:22:13 +0200

    Update

---
+++ src/components/GameResults/GameResults.tsx
@@ -0,0 +1,5 @@
+const GameResults = () => {
+  return <div>GameResults</div>;
+};
+
+export default GameResults;
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,25 @@
-// index.js
-import ReactDOM from "react-dom/client";
+// src/index.tsx
+import React from "react";
+import { createRoot } from "react-dom/client"; // Import the correct createRoot API
+import { BrowserRouter } from "react-router-dom";
 import App from "./components/App/App";
 
-const root = ReactDOM.createRoot(document.getElementById("root")!);
-root.render(<App />);
+// Get the root element from the DOM
+const container = document.getElementById("root");
+
+if (container) {
+  // Create a root.
+  const root = createRoot(container);
+
+  // Initial render
+  root.render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  );
+} else {
+  // Handle the error gracefully
+  console.error("Could not find root element to mount to!");
+}
